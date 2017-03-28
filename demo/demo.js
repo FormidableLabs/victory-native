e31adafe66fc7863945b4db830e5716d8c84b466
boygirl <lauren.eastridge@formidable.com>
2017-03-27 17:09:05 -0700

    all components render

--- demo/demo.js
+++ demo/demo.js
@@ -12,10 +12,16 @@ import Svg from "react-native-svg";
 import {
   VictoryLabel,
   VictoryChart,
+  VictoryGroup,
+  VictoryStack,
   VictoryCandlestick,
+  VictoryErrorBar,
   VictoryBar,
   VictoryLine,
   VictoryArea,
+  VictoryScatter,
+  VictoryTooltip,
+  Flyout,
   VictoryPie
 } from "victory-native";
 
@@ -105,10 +111,127 @@ export default class Demo extends Component {
         <Svg width={100} height={100}>
           <VictoryLabel x={20} y={20} text={["one", "two", "three"]} style={{fontSize: 20}}/>
         </Svg>
+        <Svg width={100} height={100}>
+          <Flyout x={20} y={10} width={20} height={20} cornerRadius={1}
+            style={{stroke: "black", fill: "none", strokeWidth: 2}} pointerLength={1} pointerWidth={1}
+          />
+        </Svg>
         <Text>{"VICTORY BAR"}</Text>
-        <VictoryBar/>
-        <VictoryCandlestick/>
-        <VictoryChart><VictoryArea/></VictoryChart>
+        <VictoryChart domain={{x: [0, 4]}}>
+          <VictoryGroup
+            labels={["a", "b", "c"]}
+            offset={10}
+            colorScale={"qualitative"}
+          >
+            <VictoryBar
+              data={[
+                {x: 1, y: 1},
+                {x: 2, y: 2},
+                {x: 3, y: 5}
+              ]}
+            />
+            <VictoryBar
+              data={[
+                {x: 1, y: 2},
+                {x: 2, y: 1},
+                {x: 3, y: 7}
+              ]}
+            />
+            <VictoryBar
+              data={[
+                {x: 1, y: 3},
+                {x: 2, y: 4},
+                {x: 3, y: 9}
+              ]}
+            />
+          </VictoryGroup>
+        </VictoryChart>
+        <VictoryChart>
+          <VictoryScatter
+            labelComponent={<VictoryTooltip/>}
+            data={[
+              {
+                x: 1, y: 3, fill: "red",
+                symbol: "plus", size: 6, label: "Red"
+              },
+              {
+                x: 2, y: 5, fill: "magenta",
+                size: 9, opacity: 0.4, label: "Magenta"
+              },
+              {
+                x: 3, y: 4, fill: "orange",
+                size: 5, label: "Orange"
+              },
+              {
+                x: 4, y: 2, fill: "brown",
+                symbol: "square", size: 6, label: "Brown"
+              },
+              {
+                x: 5, y: 5, fill: "black",
+                symbol: "triangleUp", size: 5, label: "Black"
+              }
+            ]}
+          />
+        </VictoryChart>
+        <VictoryChart animate={{duration: 1500}}>
+          <VictoryBar
+            data={this.state.transitionData}
+            style={{
+              data: {
+                fill: "tomato", width: 12
+              }
+            }}
+            animate={{
+              onExit: {
+                duration: 500,
+                before: () => ({
+                  y: 0,
+                  fill: "orange",
+                  label: "BYE"
+                })
+              }
+            }}
+          />
+        </VictoryChart>
+        <VictoryChart>
+          <VictoryStack>
+            <VictoryArea
+              data={[
+                {x: "a", y: 2}, {x: "b", y: 3}, {x: "c", y: 5}, {x: "d", y: 4}, {x: "e", y: 7}
+              ]}
+            />
+            <VictoryArea
+              data={[
+                {x: "a", y: 1}, {x: "b", y: 4}, {x: "c", y: 5}, {x: "d", y: 7}, {x: "e", y: 5}
+              ]}
+            />
+            <VictoryArea
+              data={[
+                {x: "a", y: 3}, {x: "b", y: 2}, {x: "c", y: 6}, {x: "d", y: 2}, {x: "e", y: 6}
+              ]}
+            />
+            <VictoryArea
+              data={[
+                {x: "a", y: 2}, {x: "b", y: 3}, {x: "c", y: 3}, {x: "d", y: 4}, {x: "e", y: 7}
+              ]}
+            />
+          </VictoryStack>
+        </VictoryChart>
+        <VictoryErrorBar
+          style={{
+            data: {stroke: "red", strokeWidth: 2}
+          }}
+          data={[
+            {x: 1, y: 1, errorX: [1, 0.5], errorY: .1},
+            {x: 2, y: 2, errorX: [1, 3], errorY: .1},
+            {x: 3, y: 3, errorX: [1, 3], errorY: [.2, .3]},
+            {x: 4, y: 2, errorX: [1, 0.5], errorY: .1},
+            {x: 5, y: 1, errorX: [1, 0.5], errorY: .2}
+          ]}
+        />
+        <VictoryChart><VictoryLine/></VictoryChart>
+        <VictoryChart><VictoryCandlestick data={candleData}/></VictoryChart>
+        <VictoryPie/>
         <Text>{"END"}</Text>
       </ScrollView>
     );
