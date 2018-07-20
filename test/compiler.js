f14c9dffc01bca01242840f65ec0dc3b8d634124
boygirl <lauren.eastridge@formidable.com>
2018-07-19 22:29:55 -0700

    babel 7 and compiling packages for testing

--- test/compiler.js
+++ test/compiler.js
@@ -19,9 +19,31 @@ var compile = function (fileName) {
 // We need to compile a few node_modules
 var shouldCompile = function (fileName) {
   var should = [
+    "node_modules/victory-area",
+    "node_modules/victory-axis",
+    "node_modules/victory-bar",
+    "node_modules/victory-box-plot",
+    "node_modules/victory-brush-container",
+    "node_modules/victory-brush-line",
+    "node_modules/victory-candlestick",
     "node_modules/victory-chart",
     "node_modules/victory-core",
-    "node_modules/victory-pie"
+    "node_modules/victory-create-container",
+    "node_modules/victory-cursor-container",
+    "node_modules/victory-errorbar",
+    "node_modules/victory-group",
+    "node_modules/victory-legend",
+    "node_modules/victory-line",
+    "node_modules/victory-pie",
+    "node_modules/victory-polar-axis",
+    "node_modules/victory-scatter",
+    "node_modules/victory-selection-container",
+    "node_modules/victory-shared-events",
+    "node_modules/victory-stack",
+    "node_modules/victory-tooltip",
+    "node_modules/victory-voronoi",
+    "node_modules/victory-voronoi-container",
+    "node_modules/victory-zoom-container"
   ].some(function (mod) {
     return fileName.indexOf(mod) >= 0;
   });
