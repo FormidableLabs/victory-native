0eef1ce71365c750eeb31b5813089ef6eb3bfa22
Carlos Kelly <me@carlos.dev>
2019-08-01 07:50:30 -0500

    React Native 60 upgrade (#487)
    * Upgrade iOS Demo project
    
    * Upgrade Android Demo project to use AndroidX, Hermes
    
    * Final dependency upgrades.
    
    * Re–add Babel core for CI

--- demo/metro.config.js
+++ demo/metro.config.js
@@ -1,21 +1,25 @@
-const path = require('path')
-const pkg = require('../package.json')
+const path = require("path");
+const pkg = require("../package.json");
 
 const providesModuleNodeModules = Object.keys(pkg.dependencies).concat([
-  '@babel/runtime', 'react', 'react-native', 'react-native-svg'])
+  "@babel/runtime",
+  "react",
+  "react-native",
+  "react-native-svg"
+]);
 
 module.exports = {
   projectRoot: __dirname,
-  watchFolders: [path.resolve(__dirname, '../lib')],
+  watchFolders: [path.resolve(__dirname, "../lib")],
   resolver: {
-    providesModuleNodeModules,
+    providesModuleNodeModules
   },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
+        inlineRequires: false
+      }
+    })
+  }
 };
