13caed545f6c9cae3ce55565faf91a3604a76e4e
Carlos Kelly <me@carlos.dev>
2019-08-09 11:03:25 -0500

    Fix VictoryContainer pointer events (#490)

--- demo/src/views/line-view.js
+++ demo/src/views/line-view.js
@@ -21,100 +21,98 @@ export default function LineView() {
 
   return (
     <ScrollView style={viewStyles.container}>
-      <View pointerEvents="none">
-        <VictoryLine />
+      <VictoryLine />
 
-        <VictoryLine
-          polar
-          data={[
-            { x: 0, y: 1 },
-            { x: 1, y: 3 },
-            { x: 2, y: 2 },
-            { x: 3, y: 4 },
-            { x: 4, y: 3 },
-            { x: 5, y: 5 }
-          ]}
-        />
+      <VictoryLine
+        polar
+        data={[
+          { x: 0, y: 1 },
+          { x: 1, y: 3 },
+          { x: 2, y: 2 },
+          { x: 3, y: 4 },
+          { x: 4, y: 3 },
+          { x: 5, y: 5 }
+        ]}
+      />
 
-        <VictoryLine
-          data={[
-            { amount: 1, yield: 1, error: 0.5 },
-            { amount: 2, yield: 2, error: 1.1 },
-            { amount: 3, yield: 3, error: 0 },
-            { amount: 4, yield: 2, error: 0.1 },
-            { amount: 5, yield: 1, error: 1.5 }
-          ]}
-          x={"amount"}
-          y={data => data.yield + data.error}
-        />
+      <VictoryLine
+        data={[
+          { amount: 1, yield: 1, error: 0.5 },
+          { amount: 2, yield: 2, error: 1.1 },
+          { amount: 3, yield: 3, error: 0 },
+          { amount: 4, yield: 2, error: 0.1 },
+          { amount: 5, yield: 1, error: 1.5 }
+        ]}
+        x={"amount"}
+        y={data => data.yield + data.error}
+      />
 
-        <VictoryLine y={data => Math.sin(2 * Math.PI * data.x)} />
+      <VictoryLine y={data => Math.sin(2 * Math.PI * data.x)} />
 
-        <VictoryLine
-          height={300}
-          domain={[0, 5]}
-          padding={75}
-          data={[
-            { x: 0, y: 1 },
-            { x: 1, y: 3 },
-            { x: 2, y: 2 },
-            { x: 3, y: 4 },
-            { x: 4, y: 3 },
-            { x: 5, y: 5 }
-          ]}
-          interpolation="cardinal"
-          labels={() => "LINE"}
-          style={{
-            data: {
-              stroke: "#822722",
-              strokeWidth: 3
-            },
-            labels: { fontSize: 12 }
-          }}
-        />
+      <VictoryLine
+        height={300}
+        domain={[0, 5]}
+        padding={75}
+        data={[
+          { x: 0, y: 1 },
+          { x: 1, y: 3 },
+          { x: 2, y: 2 },
+          { x: 3, y: 4 },
+          { x: 4, y: 3 },
+          { x: 5, y: 5 }
+        ]}
+        interpolation="cardinal"
+        labels={() => "LINE"}
+        style={{
+          data: {
+            stroke: "#822722",
+            strokeWidth: 3
+          },
+          labels: { fontSize: 12 }
+        }}
+      />
 
-        <VictoryLine
-          width={300}
-          style={{
-            data: {
-              stroke: data => {
-                const strokeY = data.map(d => d.y);
-                return Math.max(...strokeY) > 2 ? "red" : "blue";
-              }
+      <VictoryLine
+        width={300}
+        style={{
+          data: {
+            stroke: data => {
+              const strokeY = data.map(d => d.y);
+              return Math.max(...strokeY) > 2 ? "red" : "blue";
             }
-          }}
-          data={[
-            { x: 0, y: 1 },
-            { x: 1, y: 3 },
-            { x: 2, y: 2 },
-            { x: 3, y: 4 },
-            { x: 4, y: 3 },
-            { x: 5, y: 5 }
-          ]}
-        />
+          }
+        }}
+        data={[
+          { x: 0, y: 1 },
+          { x: 1, y: 3 },
+          { x: 2, y: 2 },
+          { x: 3, y: 4 },
+          { x: 4, y: 3 },
+          { x: 5, y: 5 }
+        ]}
+      />
 
-        <VictoryLine
-          style={{
-            data: { stroke: "red", strokeWidth: 9 }
-          }}
-          interpolation={"linear"}
-          data={[
-            { x: 0, y: 1 },
-            { x: 1, y: 3 },
-            { x: 2, y: 2 },
-            { x: 3, y: 4 },
-            { x: 4, y: 3 },
-            { x: 5, y: 5 }
-          ]}
-        />
+      <VictoryLine
+        style={{
+          data: { stroke: "red", strokeWidth: 9 }
+        }}
+        interpolation={"linear"}
+        data={[
+          { x: 0, y: 1 },
+          { x: 1, y: 3 },
+          { x: 2, y: 2 },
+          { x: 3, y: 4 },
+          { x: 4, y: 3 },
+          { x: 5, y: 5 }
+        ]}
+      />
 
-        <VictoryLine
-          style={{ data: styles }}
-          interpolation="basis"
-          animate={{ duration: 1500 }}
-          y={y}
-        />
-      </View>
+      <VictoryLine
+        style={{ data: styles }}
+        interpolation="basis"
+        animate={{ duration: 1500 }}
+        y={y}
+      />
     </ScrollView>
   );
 }
--- demo/src/views/pie-view.js
+++ demo/src/views/pie-view.js
@@ -19,76 +19,74 @@ export default function PieView() {
 
   return (
     <ScrollView style={viewStyles.container}>
-      <View pointerEvents="none">
-        <VictoryPie
-          innerRadius={75}
-          labelRadius={125}
-          style={{ labels: { fontSize: 20 } }}
-          data={data}
-          animate={{ duration: 1500 }}
-        />
+      <VictoryPie
+        innerRadius={75}
+        labelRadius={125}
+        style={{ labels: { fontSize: 20 } }}
+        data={data}
+        animate={{ duration: 1500 }}
+      />
 
-        <VictoryPie
-          style={{
-            data: {
-              stroke: "none",
-              opacity: 0.3
-            }
-          }}
-        />
-        <VictoryPie innerRadius={90} />
-        <VictoryPie endAngle={90} startAngle={-90} />
-        <VictoryPie
-          endAngle={90}
-          innerRadius={90}
-          padAngle={5}
-          startAngle={-90}
-        />
-        <VictoryPie
-          style={{
-            labels: {
-              fill: "white",
-              stroke: "none",
-              fontSize: 15,
-              fontWeight: "bold"
-            }
-          }}
-          data={[
-            { x: "<5", y: 6279 },
-            { x: "5-13", y: 9182 },
-            { x: "14-17", y: 5511 },
-            { x: "18-24", y: 7164 },
-            { x: "25-44", y: 6716 },
-            { x: "45-64", y: 4263 },
-            { x: "≥65", y: 7502 }
-          ]}
-          innerRadius={70}
-          labelRadius={100}
-          colorScale={[
-            "#D85F49",
-            "#F66D3B",
-            "#D92E1D",
-            "#D73C4C",
-            "#FFAF59",
-            "#E28300",
-            "#F6A57F"
-          ]}
-        />
-        <VictoryPie
-          style={{
-            data: {
-              stroke: d => (d.y > 75 ? "black" : "none"),
-              opacity: d => (d.y > 75 ? 1 : 0.4)
-            }
-          }}
-          data={[
-            { x: "Cat", y: 62 },
-            { x: "Dog", y: 91 },
-            { x: "Fish", y: 55 },
-            { x: "Bird", y: 55 }
-          ]}
-        />
-      </View>
+      <VictoryPie
+        style={{
+          data: {
+            stroke: "none",
+            opacity: 0.3
+          }
+        }}
+      />
+      <VictoryPie innerRadius={90} />
+      <VictoryPie endAngle={90} startAngle={-90} />
+      <VictoryPie
+        endAngle={90}
+        innerRadius={90}
+        padAngle={5}
+        startAngle={-90}
+      />
+      <VictoryPie
+        style={{
+          labels: {
+            fill: "white",
+            stroke: "none",
+            fontSize: 15,
+            fontWeight: "bold"
+          }
+        }}
+        data={[
+          { x: "<5", y: 6279 },
+          { x: "5-13", y: 9182 },
+          { x: "14-17", y: 5511 },
+          { x: "18-24", y: 7164 },
+          { x: "25-44", y: 6716 },
+          { x: "45-64", y: 4263 },
+          { x: "≥65", y: 7502 }
+        ]}
+        innerRadius={70}
+        labelRadius={100}
+        colorScale={[
+          "#D85F49",
+          "#F66D3B",
+          "#D92E1D",
+          "#D73C4C",
+          "#FFAF59",
+          "#E28300",
+          "#F6A57F"
+        ]}
+      />
+      <VictoryPie
+        style={{
+          data: {
+            stroke: d => (d.y > 75 ? "black" : "none"),
+            opacity: d => (d.y > 75 ? 1 : 0.4)
+          }
+        }}
+        data={[
+          { x: "Cat", y: 62 },
+          { x: "Dog", y: 91 },
+          { x: "Fish", y: 55 },
+          { x: "Bird", y: 55 }
+        ]}
+      />
     </ScrollView>
   );
 }
