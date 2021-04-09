a90c61a05b17bb87bdd2476dd578655625e658f9
boygirl <lauren.eastridge@formidable.com>
2021-04-08 17:53:54 -0700

    fix native specific types for VictoryContainer

--- lib/components/victory-container.d.ts
+++ lib/components/victory-container.d.ts
@@ -1,9 +1,10 @@
-import { VictoryContainer, VictoryContainerProps } from "victory-core";
+import React from "react";
+import { VictoryContainerProps } from "victory-core";
 
 export interface VictoryContainerNativeProps extends VictoryContainerProps {
-  disableContainerEvents: boolean;
-  onTouchEnd: Function;
-  onTouchStart: Function;
+  disableContainerEvents?: boolean;
+  onTouchEnd?: Function;
+  onTouchStart?: Function;
 }
 
-export default class extends VictoryContainer {}
+export default class extends React.Component<VictoryContainerNativeProps, any> {}
